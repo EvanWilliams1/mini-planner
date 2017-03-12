8ef335bdf388a00f9bb2f615855804a5880760d9
Eric P. Katz <ericpkatz@gmail.com>
2017-03-12 14:54:37 -0400

    added swig for api key

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const swig = require('swig');
+swig.setDefaults({ cache: false });
+
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+
+
 app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
-app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res, next)=> res.render('index', { API_KEY: process.env.API_KEY}));
 
 const port = process.env.PORT || 3000;
 
